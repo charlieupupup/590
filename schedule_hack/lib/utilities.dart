04ba25347f5eb16d7fd86a375f6e2d641f2f4d40
Courtney Jones <cej8@duke.edu>
2020-10-30 00:50:50 -0400

    Refactored 'HexToColor' to 'CustomColor'

--- schedule_hack/lib/utilities.dart
+++ schedule_hack/lib/utilities.dart
@@ -1,19 +1,19 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-class HexToColor extends Color {
-  static _hexToColor(String code) {
+class CustomColor extends Color {
+  static hexToColor(String code) {
     return int.parse(code.substring(1, 7), radix: 16) + 0xFF000000;
   }
 
-  HexToColor(final String code) : super(_hexToColor(code));
+  CustomColor(final String code) : super(hexToColor(code));
 }
 
-final Color colorMelon = HexToColor('#FFA69E');
-final Color colorAlmond = HexToColor('#F0E0D0');
-final Color colorBeige = HexToColor('#EAF3E0');
-final Color colorHoneydew = HexToColor('#D9F3E2');
-final Color colorAeroBlue = HexToColor('#B8F2E6');
-final Color colorPowderBlue = HexToColor('#AED9E0');
-final Color colorBlackCoral = HexToColor('#5E6472');
-final Color colorIvory = HexToColor('#F5F9F0');
+final Color colorMelon = CustomColor('#FFA69E');
+final Color colorAlmond = CustomColor('#F0E0D0');
+final Color colorBeige = CustomColor('#EAF3E0');
+final Color colorHoneydew = CustomColor('#D9F3E2');
+final Color colorAeroBlue = CustomColor('#B8F2E6');
+final Color colorPowderBlue = CustomColor('#AED9E0');
+final Color colorBlackCoral = CustomColor('#5E6472');
+final Color colorIvory = CustomColor('#F5F9F0');
