5fee28a0d8d15f58c4ede1f62745baaa91cdb9ee
Leighanne Davis <leighanne.davis@duke.edu>
2020-11-02 15:32:59 -0500

    Removed AddButton, refactor CourseList - FAB / stateful widget, popup classes

--- schedule_hack/lib/CourseList.dart
+++ schedule_hack/lib/CourseList.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:schedule_hack/AddButton.dart';
+import 'package:schedule_hack/StandardPopup.dart';
 import 'package:schedule_hack/Course.dart';
 import 'package:schedule_hack/CourseButton.dart';
 import 'package:schedule_hack/SettingsButton.dart';
+import 'package:schedule_hack/SyllabusPopup.dart';
 import 'package:schedule_hack/utilities.dart';
 
 //import 'package:schedule_hack/TabBar.dart' as T;
 
-class CourseList extends StatelessWidget {
+class CourseList extends StatefulWidget {
   CourseList();
-
+  @override
+  State<StatefulWidget> createState() {
+    return _CourseListState();
+  }
+}
+class _CourseListState extends State<CourseList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,11 +47,18 @@ class CourseList extends StatelessWidget {
       body: Container(
         child: ListView(
           children: [
-            AddButton(),
+            //AddButton(),
             CourseButton(createCourse('ECE 590: TDC'), 0),
             CourseButton(createCourse('ECE 651: Software Engineering'), 1)
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          SyllabusPopup(context);
+        },
+        backgroundColor: colorHoneydew,
+        child: Image.asset('images/add.png'),
       ),
     );
   }
