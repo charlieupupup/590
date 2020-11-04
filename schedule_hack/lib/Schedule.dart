8deb17d3648393be9ab9ff23cfa4041b1534e3f8
Guanhua Chen <charlieupupup@gmail.com>
2020-11-04 12:22:01 -0500

    add the calendar widget

--- schedule_hack/lib/Schedule.dart
+++ schedule_hack/lib/Schedule.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:schedule_hack/DateBanner.dart';
 import 'package:intl/intl.dart';
 import 'package:schedule_hack/utilities.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
+    show CalendarCarousel;
 
 class Schedule extends StatelessWidget {
   Schedule({Key key, this.title, this.date}) : super(key: key);
@@ -46,9 +49,59 @@ class Schedule extends StatelessWidget {
       ),
       body: Container(
         child: ListView(
-          children: [DateBanner(date: this.getFormattedDate())],
+          children: [DateBanner(date: this.getFormattedDate()), widget()],
         ),
       ),
     );
   }
+}
+
+Widget widget() {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 16.0),
+    child: CalendarCarousel(
+      onDayPressed: (DateTime date, List<Event> events) {
+        // this.setState(() => _currentDate = date);
+      },
+      weekendTextStyle: TextStyle(
+        color: Colors.red,
+      ),
+      thisMonthDayBorderColor: Colors.grey,
+//      weekDays: null, /// for pass null when you do not want to render weekDays
+//      headerText: Container( /// Example for rendering custom header
+//        child: Text('Custom Header'),
+//      ),
+      customDayBuilder: (
+        /// you can provide your own build function to make custom day containers
+        bool isSelectable,
+        int index,
+        bool isSelectedDay,
+        bool isToday,
+        bool isPrevMonthDay,
+        TextStyle textStyle,
+        bool isNextMonthDay,
+        bool isThisMonthDay,
+        DateTime day,
+      ) {
+        /// If you return null, [CalendarCarousel] will build container for current [day] with default function.
+        /// This way you can build custom containers for specific days only, leaving rest as default.
+
+        // Example: every 15th of month, we have a flight, we can place an icon in the container like that:
+        if (day.day == 15) {
+          return Center(
+            child: Icon(Icons.local_airport),
+          );
+        } else {
+          return null;
+        }
+      },
+      weekFormat: false,
+      // markedDatesMap: _markedDateMap,
+      height: 420.0,
+      // selectedDateTime: _currentDate,
+      daysHaveCircularBorder: false,
+
+      /// null for not rendering any border, true for circular border, false for rectangular border
+    ),
+  );
 }
