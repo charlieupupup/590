ef691a2bb274d46057c9ea59cd1e578b0e4efb20
Leighanne Davis <leighanne.davis@duke.edu>
2020-11-07 14:42:33 -0500

    Add confirm popup

---
+++ schedule_hack/lib/ConfirmPopup.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:schedule_hack/CancelButton.dart';
+import 'package:schedule_hack/CheckmarkButton.dart';
+import 'package:schedule_hack/Course.dart';
+import 'package:schedule_hack/utilities.dart';
+import 'package:flutter/material.dart';
+
+// Class to display 'standard' popup
+// text description, cancel (CancelButton.dart), and accept (CheckmarkButton.dart)
+class ConfirmPopup {
+  BuildContext context;
+  String message;
+  int index;
+  Course course = new Course();
+
+  ConfirmPopup.course(BuildContext bc, String m, int i, Course c){
+    this.course = c;
+    this.context = bc;
+    this.message = m;
+    this.index = i;
+    _showMaterialDialog2(message);
+  }
+
+  ConfirmPopup(BuildContext c, String m, int i) {
+    this.context = c;
+    this.message = m;
+    this.index = i;
+    _showMaterialDialog2(message);
+  }
+  // method for popup
+  _showMaterialDialog2(String message){
+    showDialog(context: context,
+        builder: (_) => new AlertDialog(
+          elevation: 16,
+          actions: <Widget> [
+            Center(
+              child: Text(
+                  message,
+                  style: TextStyle(fontSize: 24, color: colorBlackCoral, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center
+              ),
+            ),
+            Container(
+              child: ButtonBar(
+                alignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  CheckmarkButton(index,course)
+                ],
+              ),
+            )
+          ],
+        )
+    );
+  }
+}
