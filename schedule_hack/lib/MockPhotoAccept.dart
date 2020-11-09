551f74f7a9fb8e9c2415d78e4683c8fd636b0ba7
Leighanne Davis <leighanne.davis@duke.edu>
2020-11-09 14:04:42 -0500

    MockPhotoAccept accept button update

--- schedule_hack/lib/MockPhotoAccept.dart
+++ schedule_hack/lib/MockPhotoAccept.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:schedule_hack/Course.dart';
 import 'package:schedule_hack/utilities.dart';
 
+import 'AssignmentListHome.dart';
 import 'CancelButton.dart';
 import 'CheckmarkButton.dart';
 
 class MockPhotoAccept extends StatefulWidget {
   Course course = new Course();
 
-  MockPhotoAccept(Course c){
+  MockPhotoAccept(Course c) {
     this.course = c;
   }
   @override
@@ -19,7 +20,7 @@ class MockPhotoAccept extends StatefulWidget {
 
 class _MockPhotoAcceptState extends State<MockPhotoAccept> {
   Course course;
-  _MockPhotoAcceptState(Course c){
+  _MockPhotoAcceptState(Course c) {
     this.course = c;
   }
   @override
@@ -32,22 +33,43 @@ class _MockPhotoAcceptState extends State<MockPhotoAccept> {
       child: Column(
         children: [
           AspectRatio(
-            aspectRatio: 2.2/3,
+            aspectRatio: 2.2 / 3,
             child: Image.asset(
               'images/syllabus.png',
             ),
           ),
           Padding(
-              padding: EdgeInsets.only(
-                top: 15.0,
-              ),
-              child: ButtonBar(
-                alignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  CancelButton(),
-                  CheckmarkButton(3,this.course)
-            ],
-          ),
+            padding: EdgeInsets.only(
+              top: 15.0,
+            ),
+            child: ButtonBar(
+              alignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                CancelButton(),
+                //CheckmarkButton(3,this.course)
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => AssignmentListHome(2, this.course)//AssignmentList(),
+                      ),
+                          (route) => false,
+                    );
+                  },
+                  color: colorHoneydew,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                  child: new Text('Review',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: colorBlackCoral,
+                      fontWeight: FontWeight.bold)
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
