390fbbcdb4723b9aa3f6b0f774129bd7aa4f8cd0
Guanhua Chen <charlieupupup@gmail.com>
2020-11-11 20:47:31 -0500

    finish routing for each button

--- schedule_hack/lib/PopUp.dart
+++ schedule_hack/lib/PopUp.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:schedule_hack/Home.dart';
+import 'package:schedule_hack/SelfCare.dart';
 import 'package:schedule_hack/utilities.dart';
 
 class PopUp extends AlertDialog {
@@ -6,6 +8,7 @@ class PopUp extends AlertDialog {
   final String msg2 = 'Head to the self care tab to destress';
   final String op1 = 'Good!';
   final String op2 = 'Not Great...';
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -53,6 +56,47 @@ class MidPopUp extends PopUp {
   final String msg2 = 'How do you feel?';
   final String op1 = 'Good!';
   final String op2 = 'Not Great...';
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: colorAlmond,
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Text(
+              msg1,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              msg2,
+              textAlign: TextAlign.center,
+            ),
+            FlatButton(
+              child: Text(op1),
+              color: colorAeroBlue,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => GladPopUp()),
+                );
+              },
+            ),
+            FlatButton(
+              child: Text(op2),
+              color: colorMelon,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SadPopUp()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class GladPopUp extends PopUp {
@@ -60,6 +104,47 @@ class GladPopUp extends PopUp {
   final String msg2 = 'Head to the self care tab to destress';
   final String op1 = 'Take me there!';
   final String op2 = 'No thanks';
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: colorAlmond,
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Text(
+              msg1,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              msg2,
+              textAlign: TextAlign.center,
+            ),
+            FlatButton(
+              child: Text(op1),
+              color: colorAeroBlue,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SelfCare()),
+                );
+              },
+            ),
+            FlatButton(
+              child: Text(op2),
+              color: colorMelon,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Home(0)),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class SadPopUp extends PopUp {
@@ -67,4 +152,45 @@ class SadPopUp extends PopUp {
   final String msg2 = 'Head to the self care tab to destress';
   final String op1 = 'Take me there!';
   final String op2 = 'No thanks';
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: colorAlmond,
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Text(
+              msg1,
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              msg2,
+              textAlign: TextAlign.center,
+            ),
+            FlatButton(
+              child: Text(op1),
+              color: colorAeroBlue,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SelfCare()),
+                );
+              },
+            ),
+            FlatButton(
+              child: Text(op2),
+              color: colorMelon,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Home(0)),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
