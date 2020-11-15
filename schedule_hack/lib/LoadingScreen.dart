b670b02335e0fce681856b1f9b4fa61156cabce5
Leighanne Davis <leighanne.davis@duke.edu>
2020-11-15 14:39:13 -0500

    Loading screen updates

--- schedule_hack/lib/LoadingScreen.dart
+++ schedule_hack/lib/LoadingScreen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:schedule_hack/utilities.dart';
@@ -6,8 +7,6 @@ import 'package:schedule_hack/utilities.dart';
 import 'Home.dart';
 
 class LoadingScreen extends StatefulWidget {
-
-
   @override
   State<StatefulWidget> createState() {
     return _LoadingScreenState();
@@ -15,6 +14,22 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  List<String> list = new List<String>();
+  List<dynamic> imageList = new List<dynamic>();
+  dynamic imageResponse;
+  String inspirationString;
+
+  _LoadingScreenState(){
+    list.add("Take time to practice deep breathing while we build your schedule");
+    list.add("Try getting 6 - 8 hours of sleep to function at your best!");
+    list.add("Daily exercise can help relieve stress and make you feel better");
+    list.add("Staying hydrated helps you to learn and retain information. Try drinking 13 cups a day!");
+    imageList.add(Image.asset("images/meditation.png"));
+    imageList.add(Image.asset("images/sleepingbed.png"));
+    imageList.add(Image.asset("images/running.png"));
+    imageList.add(Image.asset("images/water.png"));
+    randomInspiration();
+  }
   @override
   void initState() {
     super.initState();
@@ -24,7 +39,31 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: initScreen(context),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: this.imageResponse,//Image.asset(this.imageString),
+            ),
+            Padding(padding: EdgeInsets.only(top: 20.0)),
+            Text(
+              this.inspirationString,
+             // "Loading",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  color: colorBlackCoral
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Padding(padding: EdgeInsets.only(top: 20.0)),
+            CircularProgressIndicator(
+              backgroundColor: colorBlackCoral,
+              strokeWidth: 1,
+            )
+          ],
+        ),
+      ),
     );
   }
 
@@ -39,31 +78,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 
-  initScreen(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              child: Image.asset("images/hack.png"),
-            ),
-            Padding(padding: EdgeInsets.only(top: 20.0)),
-            Text(
-              "Loading",
-              style: TextStyle(
-                  fontSize: 20.0,
-                  color: colorBlackCoral
-              ),
-            ),
-            Padding(padding: EdgeInsets.only(top: 20.0)),
-            CircularProgressIndicator(
-              backgroundColor: colorBlackCoral,
-              strokeWidth: 1,
-            )
-          ],
-        ),
-      ),
-    );
+  randomInspiration(){
+    Random random = new Random();
+    int randomNumber = random.nextInt(3);
+    this.inspirationString = this.list[randomNumber];
+    this.imageResponse = this.imageList[randomNumber];
   }
 }
