a229cef7c408c4a524509d2d8ec8334ef31948ee
Guanhua Chen <charlieupupup@gmail.com>
2020-11-16 23:33:13 -0500

    add layout for ScheduleElement.dart

--- schedule_hack/lib/ScheduleView.dart
+++ schedule_hack/lib/ScheduleView.dart
@@ -8,17 +8,43 @@ class ScheduleView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.all(24.0),
-        child: Row(
-          children: [Expanded(child: SchduleElement())],
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 20),
+          child: Row(
+            children: [
+              // Expanded(child: SchduleElement()),
+              Text(
+                'My day',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          width: 300,
+          height: 75,
+          decoration: BoxDecoration(color: colorPowderBlue),
         ),
-        width: 300,
-        height: 75,
-        decoration: BoxDecoration(color: colorPowderBlue),
-      ),
+        Container(
+          // margin: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              Expanded(child: SchduleElement()),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          width: 300,
+          height: 75,
+          decoration: BoxDecoration(color: colorAeroBlue),
+        ),
+        //
+      ],
     );
+    // return Column(
+    //   children: [SchduleElement()],
+    // );
   }
 }
