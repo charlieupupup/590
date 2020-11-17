7ab56d391a751af47b1b6a0e5cf122e0b74dee6b
Courtney Jones <cej8@duke.edu>
2020-11-17 12:35:34 -0500

    //returns Duration from two TimeOfDay on same Day     static Duration fromTimesOfDay(DateTime day, TimeOfDay startTime, TimeOfDay endTime)

--- schedule_hack/lib/utilities.dart
+++ schedule_hack/lib/utilities.dart
@@ -11,15 +11,24 @@ extension CalendarDateTime on DateTime {
 
 extension TimeOfDayConverter on TimeOfDay {
   //takes in string form '12:34PM' returns TimeOfDay
-  static TimeOfDay fromString(String time) {
-    int hourOffset = 0;
-    if (time.endsWith('PM')) {
-      hourOffset = 12;
+    static TimeOfDay fromString(String time) {
+      int hourOffset = 0;
+      if (time.endsWith('PM')) {
+        hourOffset = 12;
+      }
+      int hour = int.parse(time.split(':')[0]) + hourOffset;
+      int minute = int.parse(time.split(':')[1].substring(0, 2));
+      return TimeOfDay(hour: hour, minute: minute);
     }
-    int hour = int.parse(time.split(':')[0]) + hourOffset;
-    int minute = int.parse(time.split(':')[1].substring(0, 2));
-    return TimeOfDay(hour: hour, minute: minute);
-  }
+    //returns Duration from two TimeOfDay on same Day
+    static Duration fromTimesOfDay(DateTime day, TimeOfDay startTime, TimeOfDay endTime) {
+      DateTime start = DateTimeConverter.fromTimeOfDay(day, startTime);
+      DateTime end = DateTimeConverter.fromTimeOfDay(day, endTime);
+      int hours = end.hour - start.hour;
+      int minutes = end.minute - start.minute;
+      return Duration(days: 0, hours: hours, minutes: minutes);
+
+    }
 }
 
 extension DateTimeConverter on DateTime {
