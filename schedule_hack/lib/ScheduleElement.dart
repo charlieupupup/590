82c5ddd95d5acfd2d75cc3a60b270e2bfb7763fc
Guanhua Chen <charlieupupup@gmail.com>
2020-11-16 22:33:57 -0500

    add color for ScheduleElement.dart

--- schedule_hack/lib/ScheduleElement.dart
+++ schedule_hack/lib/ScheduleElement.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:schedule_hack/utilities.dart';
 
 class SchduleElement extends StatelessWidget {
   final String img = 'images/running.png';
   final String event = 'Soccer';
-  final String time = '';
+  final String time = '3-530';
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Padding(
         padding: EdgeInsets.all(10),
-        child: Row(
-          children: [
-            Image(image: AssetImage('images/running.png')),
-            Text('Soccer 3-530'),
-            Icon(Icons.arrow_right),
-          ],
+        child: Container(
+          color: colorPale,
+          child: Row(
+            children: [
+              Image(image: AssetImage(img)),
+              Text(event + ' ' + time),
+              Icon(Icons.arrow_right),
+            ],
+          ),
         ));
   }
 }
