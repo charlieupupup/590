5b56d39719307622ed7a5e01753f7040843898f8
Guanhua Chen <charlieupupup@gmail.com>
2020-11-17 20:35:12 -0500

    change button for DateBanner.dart

--- schedule_hack/lib/DateBanner.dart
+++ schedule_hack/lib/DateBanner.dart
@@ -13,27 +13,33 @@ class DateBanner extends StatelessWidget {
     return SizedBox(
       width: 300,
       height: 75,
-      child: Card(
-        margin: EdgeInsets.all(6.0),
-        elevation: 4.0,
-        color: colorMelon,
-        child: Align(
-          alignment: Alignment.center,
-          child: AutoSizeText(
-            DateFormat.yMMMMEEEEd('en_US')
-                .format(date), //Sunday, November 15, 2020
-            style: TextStyle(fontSize: 24, color: colorIvory),
-            maxLines: 1,
-            minFontSize: 20,
-            textAlign: TextAlign.center,
-            overflowReplacement: AutoSizeText(
-                DateFormat.yMMMEd('en_US').format(date), //Sun, Nov 15, 2020
-                style: TextStyle(fontSize: 24, color: colorIvory),
-                maxLines: 1,
-                minFontSize: 20.0,
-                textAlign: TextAlign.center),
-          ),
-        ),
+      // child: Card(
+      //   margin: EdgeInsets.all(6.0),
+      //   elevation: 4.0,
+      //   color: colorMelon,
+      //   child: Align(
+      //     alignment: Alignment.center,
+      //     child: AutoSizeText(
+      //       DateFormat.yMMMMEEEEd('en_US')
+      //           .format(date), //Sunday, November 15, 2020
+      //       style: TextStyle(fontSize: 24, color: colorIvory),
+      //       maxLines: 1,
+      //       minFontSize: 20,
+      //       textAlign: TextAlign.center,
+      //       overflowReplacement: AutoSizeText(
+      //           DateFormat.yMMMEd('en_US').format(date), //Sun, Nov 15, 2020
+      //           style: TextStyle(fontSize: 24, color: colorIvory),
+      //           maxLines: 1,
+      //           minFontSize: 20.0,
+      //           textAlign: TextAlign.center),
+      //     ),
+      //   ),
+      // ),
+
+      child: Row(
+        children: [
+          IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: null),
+        ],
       ),
     );
   }
