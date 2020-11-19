43d90f24bc88129f4085a936cd43023dacb74eb5
Guanhua Chen <charlieupupup@gmail.com>
2020-11-19 18:01:48 -0500

    adjust CheckNavButton.dart for ScheduleModify.dart

--- schedule_hack/lib/CheckNavButton.dart
+++ schedule_hack/lib/CheckNavButton.dart
@@ -4,15 +4,25 @@ import 'package:schedule_hack/utilities.dart';
 class CheckNavButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: null,
-      color: colorHoneydew,
-      child: Image.asset(
-        'images/checkmark.png',
-        height: 50,
-        width: 50,
-      ),
-      shape: CircleBorder(),
+    return FloatingActionButton(
+      onPressed: () {
+        showDialog(
+            context: context,
+            builder: (_) {
+              //return NewCoursePopup(nameController, startTimeController, endTimeController);
+              return;
+            });
+      },
+      backgroundColor: colorAeroBlue, //colorHoneydew,
+      child: Image.asset('images/checkmark.png'),
     );
+    // return MaterialButton(
+    //   onPressed: null,
+    //   color: colorPale,
+    //   child: Image.asset(
+    //     'images/checkmark.png',
+    //   ),
+    //   shape: CircleBorder(),
+    // );
   }
 }
