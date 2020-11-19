59c4b8c489a1b8414281d088d26436d367527a39
Guanhua Chen <charlieupupup@gmail.com>
2020-11-18 20:40:38 -0500

    base for ScheduleModify.dart

---
+++ schedule_hack/lib/ScheduleModify.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:schedule_hack/Activity.dart';
+
+class ScheduleModify extends StatelessWidget {
+  Activity _activity;
+
+  ScheduleModify(Activity activity) {
+    _activity = activity;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+}
