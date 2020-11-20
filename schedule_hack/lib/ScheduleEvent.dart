b11752d1f9cbaa825c76c8aedcf7a36389340371
Courtney Jones <cej8@duke.edu>
2020-11-20 14:15:45 -0500

    Add notes and image to ScheduleEvent.dart Created getImageFromSubject method

--- schedule_hack/lib/ScheduleEvent.dart
+++ schedule_hack/lib/ScheduleEvent.dart
@@ -1,42 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:schedule_hack/utilities.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 //primitive equivalent of Activity
 class ScheduleEvent extends Appointment {
   String start;
   String end;
-  String subject;
+  Image image;
 
-  ScheduleEvent({this.subject, this.start, this.end});
+  // ScheduleEvent(String start, String end, String subject, String notes)
+  //     : this.fromString(start, end, subject, notes);
 
-  ScheduleEvent.fromDateTime(
-      DateTime startTime, DateTime endTime, String subject)
-      : super(startTime: startTime, endTime: endTime, subject: subject) {
-    this.start = startTime.toIso8601String();
-    this.end = endTime.toIso8601String();
-    this.subject = subject;
-  }
-
-  ScheduleEvent.fromString(String start, String end, String subject)
+  ScheduleEvent(String start, String end, String subject, String notes)
       : super(
             startTime: DateTime.parse(start),
             endTime: DateTime.parse(end),
-            subject: subject) {
+            subject: subject,
+            notes: notes) {
     this.start = start;
     this.end = end;
     this.subject = subject;
+    this.image = AppImage.getImageFromSubject(subject);
   }
+
+  ScheduleEvent.fromDateTime(
+      DateTime startTime, DateTime endTime, String subject, String notes)
+      : super(
+            startTime: startTime,
+            endTime: endTime,
+            subject: subject,
+            notes: notes) {
+    this.start = startTime.toIso8601String();
+    this.end = endTime.toIso8601String();
+    this.subject = subject;
+    this.image = AppImage.getImageFromSubject(subject);
+  }
+
+  ScheduleEvent.assignment(DateTime dueDate, String subject)
+      : this.fromDateTime(
+            DateTime.now(),
+            DateTime.now().add(Duration(hours: 2)),
+            "sleep",
+            "8 hours of sleep");
 
   //test schedule event
   ScheduleEvent.test()
-      : this.fromDateTime(DateTime.now(),
-            DateTime.now().add(Duration(hours: 2)), "8 hours of sleep");
+      : this.fromDateTime(
+            DateTime.now(),
+            DateTime.now().add(Duration(hours: 2)),
+            "sleep",
+            "8 hours of sleep");
 
   //convert from json encodable
   toJSONEncodable() {
     Map<String, dynamic> m = new Map();
+    m['start'] = startTime;
+    m['end'] = endTime;
     m['subject'] = subject;
-    m['startTime'] = startTime;
-    m['endTime'] = endTime;
+    m['notes'] = notes;
     return m;
   }
 }
